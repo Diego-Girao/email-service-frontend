e81068c0e4129edd27fe6ae4b05802d7bdcb3af5
diegogirao <diegoramiro25@gmail.com>
2023-08-28 15:12:33 +0100

    fix: fix port number to 3200 at axios post

--- src/hooks/useIdentityMutation.tsx
+++ src/hooks/useIdentityMutation.tsx
@@ -3,7 +3,7 @@ import { PersonData } from "../interfaces/person-data"
 import axios from "axios"
 
 const postData = (data: PersonData) => {
-	return axios.post("http://localhost:8000", data)
+	return axios.post("http://localhost:3200/send", data)
 }
 
 export function useIdentityMutation() {
